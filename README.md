f77808ef73327c925b5d6cf1898747dca13e58df
Harvey <laozhaochaguan@gmail.com>
2020-04-15 16:52:48 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # fix-text-baseline
-Wrapper for ctx, designed to fix differences in ctx.textBaseline in different browsers
+
+> Wrapper for canvas.getContext, designed to fix differences in textBaseline in different browsers
+
+## Install
+
+Install with `npm`
+
+```bash
+$ npm install fix-text-baseline
+```
+
+Or install with `yarn`
+
+```bash
+$ yarn add fix-text-baseline
+```
+
+```js
+import fixTextBaseline from "fix-text-baseline";
+```
+
+Or umd builds are also available
+
+```html
+<script src="path/to/fix-text-baseline.js"></script>
+```
+
+Will expose the global variable to `window.fixTextBaseline`.
+
+## Usage
+
+```js
+var canvas = document.createElement("canvas");
+var ctx = fixTextBaseline(canvas.getContext("2d"));
+ctx.font = "40px Arial";
+ctx.fillText("test", 0, 10);
+```
+
+## License
+
+MIT © [Harvey Zack](https://sleepy.im/)
---
+++ fix-text-baseline.js
@@ -0,0 +1,55 @@
+(function (global, factory) {
+  typeof exports === "object" && typeof module !== "undefined"
+    ? (module.exports = factory())
+    : typeof define === "function" && define.amd
+    ? define(factory)
+    : ((global = global || self), (global.fixTextBaseline = factory()));
+})(this, function () {
+  var canvas = document.createElement("canvas");
+  canvas.style.position = "fixed";
+  canvas.style.left = "-999px";
+  canvas.style.top = "-999px";
+  canvas.width = 100;
+  canvas.height = 100;
+  document.body.appendChild(canvas);
+  var ctx = canvas.getContext("2d");
+  ctx.textBaseline = "top";
+  ctx.fillStyle = "rgb(0, 0, 0)";
+  ctx.fillRect(0, 0, 100, 100);
+  ctx.fillStyle = "rgb(255, 255, 255)";
+  ctx.fillText("T", 0, 10);
+  var result = {};
+  for (let f = 1; f < 100; f++) {
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.fillRect(0, 0, 100, 100);
+    ctx.fillStyle = "rgb(255, 255, 255)";
+    ctx.font = f + "px Arial";
+    ctx.fillText("T", 0, 10);
+    var x = (5 * f) / 10;
+    for (var y = 0; y < 20; y++) {
+      var color = ctx.getImageData(x, y, 1, 1).data;
+      if (color[0] !== 0 && color[1] !== 0 && color[2] !== 0) {
+        result[f] = y - 10;
+        break;
+      }
+    }
+  }
+  document.body.removeChild(canvas);
+  return function (ctx) {
+    var originalFillText = ctx.fillText;
+    ctx.fillText = function () {
+      var y = arguments[2];
+      if (typeof y === "number") {
+        var fontSizeMatch = ctx.font.match(/(\d+)(\.\d?)?px/i);
+        if (fontSizeMatch && fontSizeMatch[1]) {
+          var diff = result[fontSizeMatch[1]];
+          if (diff) {
+            arguments[2] = y - diff;
+          }
+        }
+      }
+      return originalFillText.apply(this, arguments);
+    };
+    return ctx;
+  };
+});
